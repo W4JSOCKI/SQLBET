11797aac30ea814a47e79eef0a04cce26209fd9a
MonkaSztuder <monika.sztuder@student.put.poznan.pl>
2023-01-21 15:56:07 +0100

    database_make

--- SQL/website/models.py
+++ SQL/website/models.py
@@ -1,18 +1,77 @@
 from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
+from sqlalchemy.orm import Mapped
 
-
-class Note(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    data = db.Column(db.String(10000))
-    date = db.Column(db.DateTime(timezone=True), default=func.now())
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-
-
-class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
+class Uzytkownik(db.Model,UserMixin):
+    id_user = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
+    discriminator = db.Column('type', db.String(50))
+    __mapper_args__ = {'polymorphic_on': discriminator}
+
+class Admin(Uzytkownik):
+    __mapper_args__ = {'polymorphic_identity': 'admin'}
+    poziom = db.Column(db.Integer)
+
+class Klient(Uzytkownik):
+    __mapper_args__ = {'polymorphic_identity': 'klient'}
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
     first_name = db.Column(db.String(150))
-    note = db.relationship('Note')
+    last_name = db.Column(db.String(150))
+
+class Kursy(db.Model):
+    kurs = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.DateTime(timezone=True), default=func.now())
+    Mecz_id_meczu = db.Column(db.Integer, db.ForeignKey('mecz.id_meczu'))
+
+class Mecz(db.Model):
+    id_meczu = db.Column(db.Integer, primary_key=True)
+    data_meczu = db.Column(db.DateTime(timezone=True), default=func.now())
+    liga = db.Column(db.String(100))
+    dr1 = db.Column(db.String(80))
+    dr2 = db.Column(db.String(80))
+    wynik_meczu = db.Column(db.String(1))
+    kurs = db.relationship('Kurs')
+    zaklad = db.relationship('Zaklad')
+
+class Zaklad(db.Model):
+    id_zakladu = db.Column(db.Integer, primary_key=True)
+    kurs = db.Column(db.Float)
+    typ = db.Column(db.String(1))
+    Kupon_id_kuponu = db.Column(db.Integer, db.ForeignKey('kupon.id_kuponu'))
+    Mecz_id_meczu = db.Column(db.Integer, db.ForeignKey('mecz.id_meczu'))
+
+class Kupon(db.Model):
+    id_kuponu = db.Column(db.Integer, primary_key=True)
+    data_zakonczenia = db.Column(db.DateTime)
+    kwota = db.Column(db.Integer)
+    kurs = db.Column(db.Float)
+    potencjalna_wygrana = db.Column(db.Float)
+    stan = db.Column(db.String(20))
+    Klient_id_user = db.Column(db.Integer, db.ForeignKey('klient.id_user'))
+
+class Portfel(db.Model):
+    id_portfela = db.Column(db.Integer, primary_key=True)
+    id_klienta = db.Column(db.Integer)
+    stan = db.Column(db.Float)
+    Klient_id_user = db.Column(db.Integer, db.ForeignKey('klient.id_user'))
+    zaklad = db.relationship('Wplata')
+    zaklad = db.relationship('Wyplata')
+
+class Wplata(db.Model):
+    id_wplaty = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.DateTime(timezone=True), default=func.now())
+    kwota = db.Column(db.Float)
+    czy_z_kuponu = db.Column(db.String(1))
+    Portfel_id_portfela = db.Column(db.Integer, db.ForeignKey('portfel.id_portfela'))
+
+
+class Wyplata(db.Model):
+    id_wypłaty = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.DateTime(timezone=True), default=func.now())
+    kwota = db.Column(db.Float)
+    czy_z_kuponu = db.Column(db.String(1))
+    Portfel_id_portfela = db.Column(db.Integer, db.ForeignKey('portfel.id_portfela'))
+
+    
