8a80e919901b79d2cfdbc1b896c51796a7e07d17
W4JSOCKI <105747490+W4JSOCKI@users.noreply.github.com>
2023-01-21 16:00:33 +0100

    portfel

--- SQL/website/views.py
+++ SQL/website/views.py
@@ -24,14 +24,3 @@ def home():
     return render_template("home.html", user=current_user)
 
 
-@views.route('/delete-note', methods=['POST'])
-def delete_note():
-    note = json.loads(request.data)
-    noteId = note['noteId']
-    note = Note.query.get(noteId)
-    if note:
-        if note.user_id == current_user.id:
-            db.session.delete(note)
-            db.session.commit()
-
-    return jsonify({})
