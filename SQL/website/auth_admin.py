81d7951952e7aafb5dbc4d11575a0b0ac3844dbd
MonkaSztuder <monika.sztuder@student.put.poznan.pl>
2023-01-22 14:45:03 +0100

    admin login

--- SQL/website/auth_admin.py
+++ SQL/website/auth_admin.py
@@ -35,30 +35,3 @@ def logout():
     return redirect(url_for('auth_admin.login'))
 
 
-@auth_admin.route('/sign-up-admin', methods=['GET', 'POST'])
-def sign_up():
-    if request.method == 'POST':
-        email = request.form.get('email')
-        poziom = request.form.get('poziom')
-        password1 = request.form.get('password1')
-        password2 = request.form.get('password2')
-
-        user = Admin.query.filter_by(email=email).first()
-        if user:
-            flash('Email already exists.', category='error')
-        elif len(email) < 4:
-            flash('Email must be greater than 3 characters.', category='error')
-        elif password1 != password2:
-            flash('Passwords don\'t match.', category='error')
-        elif len(password1) < 7:
-            flash('Password must be at least 7 characters.', category='error')
-        else:
-            new_user = Admin(email=email,poziom=poziom, password=generate_password_hash(
-                password1, method='sha256'))
-            db.session.add(new_user)
-            db.session.commit()
-            login_user(new_user, remember=True)
-            flash('Account created!', category='success')
-            return redirect(url_for('views.home'))
-
-    return render_template("sign_up_admin.html", user=current_user)
