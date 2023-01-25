3c2c30c51e56554b10c3a63c45edee4904d0a3d4
MonkaSztuder <monika.sztuder@student.put.poznan.pl>
2023-01-25 17:18:14 +0100

    edycja kursu

--- SQL/website/opcje_admina.py
+++ SQL/website/opcje_admina.py
@@ -158,6 +158,7 @@ def dodaj_kurs():
     conn = db.engine.connect()
     sql = select(Mecz.id_meczu, Mecz.liga, Mecz.data_meczu, Mecz.dr1, Mecz.dr2)
     alll = conn.execute(sql).fetchall()
+
     if request.method == 'POST':
 
         kurs1 = request.form.get("kurs1")
@@ -178,3 +179,28 @@ def dodaj_kurs():
     return render_template("dod_kurs.html", user=current_user,Mecze=alll)
 
 
+@dod_admin.route('/zmkurs', methods=['GET', 'POST'])
+def edytuj_kurs():
+    conn = db.engine.connect()
+    sql = select(Mecz.id_meczu, Mecz.liga, Mecz.data_meczu, Mecz.dr1, Mecz.dr2,Kursy.Mecz_id_meczu,Kursy.kurs1,Kursy.kurs2,Kursy.kursx,Kursy.id_kursu,Kursy.data).join(Kursy).where(Mecz.wynik_meczu==None)
+    ligi = conn.execute(sql).fetchall()
+    print(ligi)
+
+    if request.method == 'POST':
+
+        kurs1 = request.form.get("kurs1")
+        kurs2 = request.form.get("kurs2")
+        kursx = request.form.get("kursx")
+        data = date.today()
+        # data=datetime.now()
+        id_meczu = request.form.get("game")
+        print(kurs1, kursx, kurs2, data, id_meczu)
+        if id_meczu == None:
+            flash("Musisz Wybrac mecz!", category='error')
+        else:
+            flash("Succes: Kurs zmieniony! ", category='success')
+            sql = insert(Kursy).values(kurs1=kurs1, kurs2=kurs2, kursx=kursx, data=data, Mecz_id_meczu=id_meczu)
+            conn.execute(sql)
+            return redirect(url_for('views.home_admin'))
+
+    return render_template("edit_kurs.html", user=current_user,Mecze=ligi)
